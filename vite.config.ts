1e23d7a3a5cf19c4ad964bb1766a4de8a2b37ead
Kyle Mathews <mathews.kyle@gmail.com>
2023-12-04 16:31:10 -0800

    Ok... finally got env var passing worked out

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react-swc"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    VITE_ELECTRIC_URL: JSON.stringify(process.env.VITE_ELECTRIC_URL),
+  },
   assetsInclude: [`**/*.wasm`],
 })
