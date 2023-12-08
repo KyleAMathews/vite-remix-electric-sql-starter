0b63b77cc9bebcd02d8d193a1b14812e659dacf1
Kyle Mathews <mathews.kyle@gmail.com>
2023-12-08 14:48:35 -0800

    Fix cleanup-example-code script

--- scripts/cleanup-example-code.js
+++ scripts/cleanup-example-code.js
@@ -1,7 +1,7 @@
 const shell = require(`shelljs`)
 
-shell.rm(`db/migrations/002-create-contacts.sql`)
-shell.rm(`db/migrations/003-create-favorite-contacts.sql`)
+shell.rm(`db/migrations/001-create-contacts.sql`)
+shell.rm(`db/migrations/002-create-favorite-contacts.sql`)
 shell.rm(`src/daos/contacts.ts`)
 shell.rm(`src/index.css`)
 shell.touch(`src/index.css`)
