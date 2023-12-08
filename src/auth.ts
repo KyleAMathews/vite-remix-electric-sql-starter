d6878a8163f78f9b20745c6046d519598c046381
Kyle Mathews <mathews.kyle@gmail.com>
2023-12-08 09:09:32 -0800

    Don't hardcode the electric image

--- src/auth.ts
+++ src/auth.ts
@@ -3,7 +3,7 @@ import { genUUID } from "electric-sql/util"
 
 // This is just a demo. In a real app, the user ID would
 // usually come from somewhere else :)
-export const dummyUserId = `d4f54ff3-5c5c-4898-ae30-4f3ee630e5c9`
+export const dummyUserId = genUUID() //`d4f54ff3-5c5c-4898-ae30-4f3ee630e5c9`
 
 // Generate an insecure authentication JWT.
 // See https://electric-sql.com/docs/usage/auth for more details.
